ac865027caaba57af8c600491fa0fe1f08d5702b
Maximiliam Viklund TE22E <143697940+MaximiliamViklund@users.noreply.github.com>
2024-10-09 11:14:42 +0200

    Movement

--- Assets/Levels/Scripts/Movement.cs
+++ Assets/Levels/Scripts/Movement.cs
@@ -9,14 +9,17 @@ public class Movement : MonoBehaviour
 
 
 
-    private void Update() {
-        
-        body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, body.velocity.y);
-
-        if(Input.GetKey(KeyCode.Space)){
-            body.velocity = new Vector2(body.velocity.x, speed);
+    private void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(xInput, yInput);
+        if (movement.magnitude > 0)
+        {
+            movement.Normalize();
         }
 
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 
 
